95248db73862ab328975c72b34e7699398ca3061
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-06-30 15:35:10 +0530

    'Login_App'

--- all login/src/App.js
+++ all login/src/App.js
@@ -1,48 +1,63 @@
 import React, { useState } from 'react';
+import reactDom from 'react-dom';
 import './App.css';
 
-const App=()=> {
-  const [fname,setFname]=useState(" ");
-  const SubmitForm=(event)=>
-  {
-   
-    console.log('you have submitted this form')
-  }
+const App = () => {
+  const [fname, setFname] = useState(" ");
+  const [lname, setLname] = useState(' ');
 
-  const ChangeInput =(event) =>
-  {
-    console.log('it change the state')
+
+  const ChangeInput = (event) => {
+
     console.log(event.target.value)
     setFname(event.target.value)
   }
+  const ChangeInputTwo = (event) => {
+    console.log(event.target.value);
+    setLname(event.target.value)
+  }
+
+  const onSubmits = (event) => {
+    event.preventDefault();
+    setFname(fname)
+    setLname(lname)
+  }
   return (
     <>
-    <h1 >Login Template</h1>
-      <h1 >Hello</h1>
-      <input
-        style={{
-          marginLeft: '45%'
-        }}
-        onChange={ChangeInput}
-        type='text'
-        placeholder='Enter your first name'
-        value=''
-      />
-      <input
-        style={{
-          marginLeft: '45%',
-          marginTop: '10%'
-        }}
-        type='text'
-        placeholder='Enter your last name'
-      />
-      <br />
-      <button 
-      style={{
-        marginLeft: '50%',
-        marginTop: '2%'
-      }}
-      type='submit' onClick={SubmitForm}>Submit</button>
+      <div>
+        <form onSubmit={onSubmits}>
+        <div>
+          <h1 >Login Template</h1>
+          <h1 >Hello {fname} {lname} </h1>
+          
+            <input
+              style={{
+                marginLeft: '45%'
+              }}
+              onChange={ChangeInput}
+              type='text'
+              placeholder='Enter your first name'
+              value={fname}
+            />
+            <input
+              style={{
+                marginLeft: '45%',
+                marginTop: '10%'
+              }}
+              onChange={ChangeInputTwo}
+              type='text'
+              placeholder='Enter your last name'
+            />
+          </div>
+          <br />
+          <button
+            style={{
+              marginLeft: '50%',
+              marginTop: '2%'
+            }}
+            type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   );
 }
